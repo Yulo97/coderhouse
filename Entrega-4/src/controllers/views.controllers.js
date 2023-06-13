e8864c9d083cef0be5eac2c0954f79bdf9b2a24c
Yulo97 <giuliano_devito@hotmail.com>
2023-06-12 21:03:17 -0300

    eliminacion de entrega 4

--- Entrega-4/src/controllers/views.controllers.js
+++
@@ -1,8 +0,0 @@
-import { ProductManager } from "../ProductManager.js";
-
-const manager = new ProductManager("./products.json");
-
-export const getProducts = async (req, res) => {
-    const products = await manager.getProducts()
-    res.render('index', { products })
-};
--- Entrega-4/src/routes/views.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as viewsControllers from "../controllers/views.controllers.js"
-
-const router = Router()
-
-router.get('/', viewsControllers.getProducts)
-
-export default router
