7752c522cf776fa108a6ec14c2432c63ab4dde41
Yulo97 <giuliano_devito@hotmail.com>
2023-11-08 19:04:18 -0300

    actualizacion

--- Entrega/src/passport-config.js
+++ Entrega/src/passport-config.js
@@ -1,105 +1,122 @@
-import passport from "passport"
-import LocalStrategy from "passport-local"
-import GitHubStrategy from "passport-github2"
-import userModel from "./models/user.model.js"
-import { passwordCompare, passwordEncrypt } from "./utils/password.js"
-import config from "./config.js"
-import CustomErrors from "./services/errors/CustomError.js"
-import { generateUserInfo } from "./services/errors/info.js"
-import EErrors from "./services/errors/enums.js"
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import GitHubStrategy from "passport-github2";
+import userModel from "./models/user.model.js";
+import { passwordCompare, passwordEncrypt } from "./utils/password.js";
+import config from "./config.js";
+import CustomErrors from "./services/errors/CustomError.js";
+import { generateUserInfo } from "./services/errors/info.js";
+import EErrors from "./services/errors/enums.js";
 
-passport.use(new LocalStrategy(async (username, password, done) => {
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
     try {
-        const result = await userModel.findOne({ email: username })
+      const result = await userModel.findOne({ email: username });
 
-        if (!result) return done(null, false, { message: "Usuario no encontrado" })
+      if (!result) return done(null, false, { message: "Usuario no encontrado" });
 
-        if (!passwordCompare(password, result.password)) return done(null, false, { message: "Contraseña incorrecta" })
+      if (!passwordCompare(password, result.password))
+        return done(null, false, { message: "Contraseña incorrecta" });
 
-        return done(null, result)
+      return done(null, result);
     } catch (error) {
-        done(error)
+      done(error);
     }
-}))
+  })
+);
 
-passport.use('register', new LocalStrategy({
-    passReqToCallback: true,
-    usernameField: 'email'
-}, async (req, username, password, done) => {
-    const { first_name, last_name, email, age } = req.body
+passport.use(
+  "register",
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+      usernameField: "email",
+    },
+    async (req, username, password, done) => {
+      const { first_name, last_name, email, age } = req.body;
 
-    if (!first_name || !last_name || !email) {
+      if (!first_name || !last_name || !email) {
         try {
-            CustomErrors.createError({
-                name: "User creation error",
-                cause: generateUserInfo({ first_name, last_name, email }),
-                message: "Error trying to create User",
-                code: EErrors.INVALID_TYPES_ERROR
-            })
+          CustomErrors.createError({
+            name: "User creation error",
+            cause: generateUserInfo({ first_name, last_name, email }),
+            message: "Error trying to create User",
+            code: EErrors.INVALID_TYPES_ERROR,
+          });
         } catch (error) {
-            console.error("Se produjo un error: ", error.message)
+          console.error("Se produjo un error: ", error.message);
         }
-    }
+      }
 
-    try {
-        const user = await userModel.findOne({ email: username })
+      try {
+        const user = await userModel.findOne({ email: username });
         if (user) {
-            console.log('User already exists')
-            return done(null, false)
+          console.log("User already exists");
+          return done(null, false);
         }
 
         const newUser = {
-            first_name, last_name, email, age, password: passwordEncrypt(password)
-        }
+          first_name,
+          last_name,
+          email,
+          age,
+          password: passwordEncrypt(password),
+        };
 
-        const result = await userModel.create(newUser)
-        return done(null, result)
-    } catch (error) {
-        return done(error)
+        const result = await userModel.create(newUser);
+        return done(null, result);
+      } catch (error) {
+        return done(error);
+      }
     }
-}))
+  )
+);
 
-passport.use('github', new GitHubStrategy({
-    clientID: config.passportIdGithub,
-    clientSecret: config.passportClientSecretGithub,
-    callbackURL: config.passportCallbackUrlGithub
-
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
-        const user = await userModel.findOne({ email: profile._json.email })
-        if (user) return done(null, user)
+passport.use(
+  "github",
+  new GitHubStrategy(
+    {
+      clientID: config.passportIdGithub,
+      clientSecret: config.passportClientSecretGithub,
+      callbackURL: config.passportCallbackUrlGithub,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const user = await userModel.findOne({ email: profile._json.email });
+        if (user) return done(null, user);
 
         const newUser = await userModel.create({
-            email: profile._json.email,
-            first_name: profile._json.name,
-            last_name: " ",
-            age: 0,
-            password: " "
-        })
+          email: profile._json.email,
+          first_name: profile._json.name,
+          last_name: " ",
+          age: 0,
+          password: " ",
+        });
 
-        return done(null, newUser)
-    } catch (error) {
-        done(error)
+        return done(null, newUser);
+      } catch (error) {
+        done(error);
+      }
     }
-}
-));
+  )
+);
 
 // serializacion
 passport.serializeUser((user, done) => {
-    done(null, user.id)
-})
+  done(null, user.id);
+});
 
 // deserializacion
 passport.deserializeUser(async (id, done) => {
-    try {
-        const result = await userModel.findById(id)
+  try {
+    const result = await userModel.findById(id);
 
-        if (!result) return done(null, false, { message: "Usuario no encontrado" })
+    if (!result) return done(null, false, { message: "Usuario no encontrado" });
 
-        done(null, result)
-    } catch (error) {
-        done(error);
-    }
-})
+    done(null, result);
+  } catch (error) {
+    done(error);
+  }
+});
 
 export default passport;
--- Entrega/src/routes/user.routes.js
+++ Entrega/src/routes/user.routes.js
@@ -1,44 +1,59 @@
 import { Router } from "express";
-import * as controllers from "../controllers/user.controllers.js"
+import * as controllers from "../controllers/user.controllers.js";
 import passport from "passport";
 import { lastConnection } from "../middlewares/auth.middleware.js";
-import { uploadDocument } from "../middlewares/multer.middleware.js"
+import { uploadDocument } from "../middlewares/multer.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.post('/register', passport.authenticate('register', {
-    successRedirect: '/api/views/products',
-    failureRedirect: '/api/views/login',
-    failureFlash: true
-}));
-
-
-router.post('/login', passport.authenticate('local', {
+router.post(
+  "/register",
+  passport.authenticate("register", {
     successRedirect: "/api/views/products",
-    failureRedirect: "/api/views/login"
-}), lastConnection)
+    failureRedirect: "/api/views/login",
+    failureFlash: true,
+  })
+);
 
-router.get('/login-github', lastConnection , passport.authenticate('github', {
-    scope: ['email'],
-}))
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/api/views/products",
+    failureRedirect: "/api/views/login",
+  }),
+  lastConnection
+);
 
-router.get('/callbackgithub', passport.authenticate('github', {
-    failureRedirect: '/api/views/login'
-}), lastConnection , controllers.redirectProducts)
+router.get(
+  "/login-github",
+  lastConnection,
+  passport.authenticate("github", {
+    scope: ["email"],
+  })
+);
 
-router.post('/logout', lastConnection ,controllers.logoutUser)
+router.get(
+  "/callbackgithub",
+  passport.authenticate("github", {
+    failureRedirect: "/api/views/login",
+  }),
+  lastConnection,
+  controllers.redirectProducts
+);
 
-router.post('/restore-password', controllers.restorePassword1)
-router.post('/reset-password', controllers.resetPassword)
+router.post("/logout", lastConnection, controllers.logoutUser);
 
-router.put('/premium/:uid', controllers.updateRole)
+router.post("/restore-password", controllers.restorePassword1);
+router.post("/reset-password", controllers.resetPassword);
 
-router.post('/:uid/documents', uploadDocument, (req, res) => {
-    res.json({status: "success", message: "Documento subido correctamente"})
-})
+router.put("/premium/:uid", controllers.updateRole);
 
-router.get("/", controllers.getUsers)
+router.post("/:uid/documents", uploadDocument, (req, res) => {
+  res.json({ status: "success", message: "Documento subido correctamente" });
+});
 
-router.delete("/", controllers.deleteInactiveUsers)
+router.get("/", controllers.getUsers);
 
-export default router
+router.delete("/", controllers.deleteInactiveUsers);
+
+export default router;
